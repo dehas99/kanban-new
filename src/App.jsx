12191c4d41393fec0e499294ba02657e5910479f
dehas99 <dehas99@gmail.com>
2023-01-23 10:40:55 +0100

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SideNav from "./components/SideNav/SideNav";
+import "./SideNav.scss";
+
+function App() {
+  return;
+  <div className="App">
+    <SideNav />
+  </div>;
+}
+
+export default App;
---
+++ src/components/SideNav/SideNav.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+export default function SideNav() {
+  return (
+    <div className="sidenav">
+      <p>All BOARDS (2)</p>
+    </div>
+  );
+}
