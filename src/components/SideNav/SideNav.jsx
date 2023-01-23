8f0e1549606c5e9de942a9716a0128bbae14316f
dehas99 <dehas99@gmail.com>
2023-01-23 11:17:45 +0100

    added sidebar

--- src/components/SideNav/SideNav.jsx
+++ src/components/SideNav/SideNav.jsx
@@ -3,6 +3,9 @@ export default function SideNav() {
   return (
     <div className="sidenav">
       <p>All BOARDS (2)</p>
+      <ul>
+        <li className="active">Kanban board</li>
+      </ul>
     </div>
   );
 }
